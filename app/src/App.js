e51fafbaba3dfad9e761bf83fd5c023ee5c1afba
Pavel Tyukin <tyukin.pavel@yandex.ru>
2021-10-06 09:13:50 +0300

    lesson 1

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Message from './components/Message';
+
+const myMessageText = 'My message';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{ textAlign: 'center' }}>
+        <Message
+          text={ myMessageText }
+        />
+      </div>
+    </>
   );
 }
 
---
+++ app/src/components/Message.js
@@ -0,0 +1,10 @@
+
+function Message(props) {
+  return (
+    <div>
+      <span style={{ backgroundColor: 'red' }}>{ props.text }</span>
+    </div>
+  );
+}
+
+export default Message;
